abc8313440204e2f18085d837a18183508fa84b6
Envios Dosruedas <enviosdosruedas@gmail.com>
2025-11-02 06:32:05 +0000

    Error:  ⚠ Cross origin request detected from 6000-firebase-studio-175383

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-          ignoreBuildErrors: true,
-  },
-  eslint: {
-          ignoreDuringBuilds: true,
-  },
-  images: {
-          remotePatterns: [
-                  {
-                          protocol: 'https',
-                          hostname: 'placehold.co',
-                          port: '',
-                          pathname: '/**',
-                  },
-                  {
-                          protocol: 'https',
-                          hostname: 'images.unsplash.com',
-                          port: '',
-                          pathname: '/**',
-                  },
-                  {
-                          protocol: 'https',
-                          hostname: 'picsum.photos',
-                          port: '',
-                          pathname: '/**',
-                  },
-          ],
+  experimental: {
+    allowedDevOrigins: ["https://6000-firebase-studio-1753830952443.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev"],
   },
 };
 
