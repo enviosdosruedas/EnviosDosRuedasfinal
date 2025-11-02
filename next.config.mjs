bc3dcbbaa435823592ea5f33f630f7d99a4e31f7
Envios Dosruedas <enviosdosruedas@gmail.com>
2025-11-02 06:35:45 +0000

    rror:  ⚠ Invalid next.config.mjs options detected: 2025-11-02T06:35:00Z

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    allowedDevOrigins: ["https://6000-firebase-studio-1753830952443.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev"],
+    // Keep other experimental features if any, but remove allowedDevOrigins from here.
   },
+  // Add allowedDevOrigins to the root of the config object
+  allowedDevOrigins: [
+    "https://6000-firebase-studio-1753830952443.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev",
+  ],
 };
 
 export default nextConfig;
